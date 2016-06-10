c8029cc989fffe3ebb8df9c241494dfe7203bb89
liujshi <liujunshisysu@gmail.com>
2016-06-10 11:56:05 +0800

    cc

---
+++ leetcode/Profiler.cpp
@@ -0,0 +1,49 @@
+#include "Profiler.h"
+namespace Profiler{
+
+	ProfilerInstance::TimePoint ProfilerInstance::startTime;
+	ProfilerInstance::TimePoint ProfilerInstance::finishTime;
+	ProfilerInstance::DurationTime ProfilerInstance::duringTime;
+
+	void ProfilerInstance::start(){
+		startTime = SteadyClock::now();
+	}
+
+	void ProfilerInstance::finish(){
+		finishTime = SteadyClock::now();
+		duringTime = std::chrono::duration_cast<DurationTime>(finishTime - startTime);
+	}
+
+	void ProfilerInstance::dumpDuringTime(std::ostream& os){
+		os << "total " << duringTime.count() * 1000 << " milliseconds" << std::endl;
+	}
+
+	double ProfilerInstance::second(){
+		return duringTime.count();
+	}
+
+	double ProfilerInstance::millisecond(){
+		return duringTime.count() * 1000;
+	}
+
+	size_t ProfilerInstance::memory(MemoryUnit mu){
+		size_t memory = 0;
+		PROCESS_MEMORY_COUNTERS pmc;
+		HANDLE hProcess = GetCurrentProcess();
+		if (!GetProcessMemoryInfo(hProcess, &pmc, sizeof(pmc)))
+			throw std::runtime_error("GetProcessMemoryInfo failed");
+		CloseHandle(hProcess);
+		switch (mu){
+		case MemoryUnit::KB_:
+			memory = pmc.WorkingSetSize KB;
+			break;
+		case MemoryUnit::MB_:
+			memory = pmc.WorkingSetSize MB;
+			break;
+		case MemoryUnit::GB_:
+			memory = pmc.WorkingSetSize GB;
+			break;
+		}
+		return memory;
+	}
+}
--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -4,8 +4,4 @@
 #include "stdafx.h"
 
 
-int _tmain(int argc, _TCHAR* argv[])
-{
-	return 0;
-}
 
